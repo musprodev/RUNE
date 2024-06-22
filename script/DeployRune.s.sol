abfff3970583d73cdf10979abddd2ec4106f8b98
musprodev <musprodev@outlook.com>
2024-06-22 18:09:55 +0100

    deploy script fix

--- script/DeployRune.s.sol
+++ script/DeployRune.s.sol
@@ -4,16 +4,14 @@ pragma solidity ^0.8.26;
 import {Script} from "forge-std/Script.sol";
 import {RuneToken} from "../src/RuneToken.sol";
 
-contract DeployRuneToken is Script {
-    function setUp() public {}
+contract DeployRune is Script {
+    function run() external {
+        uint256 initialSupply = 1_000_000 * 1e18;  // Adjust as needed
+        address siteOwner = 0xf53a204f13408Ad8B0ed1c5292F78A1cb42BFF97; // Replace with the actual site owner address
 
-    function run() public {
         vm.startBroadcast();
 
-        uint256 initialSupply = 1000 * 10**18;
-        address siteOwner = 0xf53a204f13408Ad8B0ed1c5292F78A1cb42BFF97; // replace with actual siteOwner address
-
-        RuneToken runeToken = new RuneToken(initialSupply, siteOwner);
+        new RuneToken(initialSupply, siteOwner);
 
         vm.stopBroadcast();
     }
